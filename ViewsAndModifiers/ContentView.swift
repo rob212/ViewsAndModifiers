e90cc4d0855c95d5df1a98276d0e207402168751
Rob McBryde <robertmcbryde83@gmail.com>
2025-01-23 22:00:36 +0000

    Initial Commit

---
+++ ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Rob McBryde on 23/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
